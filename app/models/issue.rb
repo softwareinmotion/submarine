45fe9d690728733deed9364558a55b6606e92d30
Barbara Hönle <barbara.hoenle@softwareinmotion.de>
2012-01-31 15:47:03 +0100

    Make issue types selectable

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,6 +3,10 @@ class Issue < ActiveRecord::Base
   
   validates_presence_of :name, :type
 
+  def self.children_types
+    [['Bug', :bug], ['Task', :task], ['UserStory', :user_story]]
+  end
+
   def formatted_story_points
     self.story_points.to_i == self.story_points ? story_points.to_i.to_s : '1/2'
   end
