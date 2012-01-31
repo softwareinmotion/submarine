9e7ea18b48a08b63c41db8bc4ffd40e223e3afd1
Barbara Hönle <barbara.hoenle@softwareinmotion.de>
2012-01-31 15:29:18 +0100

    Add styling files

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,8 @@ class Issue < ActiveRecord::Base
   belongs_to :project
   
   validates_presence_of :name, :type
+
+  def formatted_story_points
+    self.story_points.to_i == self.story_points ? story_points.to_i.to_s : '1/2'
+  end
 end
