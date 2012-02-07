18a92f5a85053388cbb7b2a842d1d892e4cce614
Julien Gantner <Julien.Gantner@softwareinmotion.de>
2012-02-07 15:44:38 +0100

    adds a datamigration to prepare existing data to work with the drag'n'drop sorting

---
+++ db/migrate/20120207143114_adds_the_list_order_issues_already_in_the_database.rb
@@ -0,0 +1,13 @@
+class AddsTheListOrderIssuesAlreadyInTheDatabase < ActiveRecord::Migration
+  def up
+    issues = Issue.all
+    issues[0].update_attributes(:predecessor_id => nil)
+    for i in 1...issues.length
+      issues[i].update_attributes(:predecessor_id => issues[i-1].id)
+    end
+  end
+
+  def down
+    Issue.all.each{|i| i.update_attributes(:predecessor_id => nil)}
+  end
+end
