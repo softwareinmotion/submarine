fe1a5ba5284457813695f96f641a36f1b6c23cc1
Julien Gantner <Julien.Gantner@softwareinmotion.de>
2012-02-07 15:53:54 +0100

    makes the last migration aware of an empty db

--- db/migrate/20120207143114_adds_the_list_order_issues_already_in_the_database.rb
+++ db/migrate/20120207143114_adds_the_list_order_issues_already_in_the_database.rb
@@ -1,7 +1,7 @@
 class AddsTheListOrderIssuesAlreadyInTheDatabase < ActiveRecord::Migration
   def up
     issues = Issue.all
-    issues[0].update_attributes(:predecessor_id => nil)
+    issues[0].update_attributes(:predecessor_id => nil) if issues[0]
     for i in 1...issues.length
       issues[i].update_attributes(:predecessor_id => issues[i-1].id)
     end
