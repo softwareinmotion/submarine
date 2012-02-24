ed9ec1af1d36c379d2c85d9a59af1dcc041957de
Marcus Seel <marcus.seel@softwareinmotion.de>
2012-02-24 15:13:18 +0100

    adds drag&drop feature - not finished

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,6 @@ module Submarine
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
   end
 end
