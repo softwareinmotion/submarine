6418e4c31ff0aed9b0758a7f8b24776f21afefc4
Sabrina Lang <sabrina.lang@softwareinmotion.de>
2012-02-29 09:22:06 +0100

    implements drag n drop for backlog items - not finsihed

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -28,8 +28,19 @@ class Issue < ActiveRecord::Base
     backlog_list.each_with_index do |id, i|
       Issue.find(id).update_attributes!(:predecessor_id => (i == 0 ? nil : backlog_list[i-1]), :sprint_flag => false)
     end
+    if backlog_list.length > 0
+      first_backlog_issue = Issue.find_by_id backlog_list.first
+      first_backlog_issue.predecessor_id = nil
+      first_backlog_issue.save
+    end 
+    
     sprint_backlog_list.each_with_index do |id, i|
       Issue.find(id).update_attributes!(:predecessor_id => (i == 0 ? nil : sprint_backlog_list[i-1]), :sprint_flag => true)
+    end
+    if sprint_backlog_list.length > 0
+      first_sprint_backlog_issue = Issue.find_by_id sprint_backlog_list.first
+      first_sprint_backlog_issue.predecessor_id = nil
+      first_sprint_backlog_issue.save
     end
   end
 
