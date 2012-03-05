70a6dab7cb75bf75e4ebcce7255d32aed4b23964
Julien Gantner <Julien.Gantner@softwareinmotion.de>
2012-03-05 11:28:47 +0100

    completes the drag'n'drop task

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,21 +1,25 @@
 FactoryGirl.define do
   factory :bug do
     sequence(:name) {|n| "bug##{n}"}
+    sequence(:description) {|n| "desc#{n}"}
     association :project
   end
 
   factory :task do
     sequence(:name) {|n| "task##{n}"}
+    sequence(:description) {|n| "desc#{n}"}
     association :project
   end
 
   factory :user_story do
     sequence(:name) {|n| "user_story#{n}"}
+    sequence(:description) {|n| "desc#{n}"}
     association :project
   end
   
   factory :issue do
     sequence(:name) {|n| "issue#{n}"}
+    sequence(:description) {|n| "desc#{n}"}
     type "Task"
     association :project
   end
