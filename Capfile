ec37f1aa922a52745202d0e226009be2fac2f0de
Sabrina Lang <sabrina.lang@softwareinmotion.de>
2012-03-06 13:22:07 +0100

    adds Capistrano settings

---
+++ Capfile
@@ -0,0 +1,15 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.2@submarine'
+
+# bundler bootstrap
+require 'bundler/capistrano'
+
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+# Uncomment if you are using Rails' asset pipeline
+load 'deploy/assets'
+
+Dir['vendor/gems/*/recipes/*.rb','vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+
+load 'config/deploy' # remove this line to skip loading any of the default tasks
---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :rails_env, 'production'
+set :application, "submarine"
+set :deploy_to, "/var/lib/submarine"
+set :use_sudo, false
+default_run_options[:pty] = true
+set :rake,      "bundle exec rake"
+
+set :scm, :git
+set :repository,  "ssh://entadmin@192.168.202.4/home/Projects/submarine.git"
+
+
+role :web, "ent"                          # Your HTTP server, Apache/etc
+role :app, "ent"                          # This may be the same as your `Web` server
+role :db,  "ent", :primary => true # This is where Rails migrations will run
+
+namespace :deploy do
+  task :start, :roles => [:web, :app] do 
+    run "cd #{deploy_to}/current && nohup bundle exec thin -C thin/production_config.yml -R thin/config.ru start" 
+  end
+  task :stop, :roles => [:web, :app] do 
+    run "cd #{deploy_to}/current && nohup bundle exec thin -C thin/production_config.yml -R thin/config.ru stop"
+  end
+#  task :restart, :roles => [:web, :app], :except => { :no_release => true } do
+#    deploy.stop
+#    deploy.start
+#  end
+end
