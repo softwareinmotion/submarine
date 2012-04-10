020752175ff6090d751d6a1ab939eab8dc61671a
Marcus Seel <marcus.seel@softwareinmotion.de>
2012-04-10 15:44:27 +0200

    changes some paths

--- config/deploy/ent.rb
+++ config/deploy/ent.rb
@@ -1,7 +1,6 @@
 ssh_options[:user] = "administrator"
 set :domain, "ent"
 set :branch, 'multistage'
-set :deploy_to, "/var/lib/#{application}"
 role :web,  domain 
 role :app,  domain 
 role :db, domain, :primary => true
