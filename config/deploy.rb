29c3f52cd313bbb304415a3f20db57a9e7d38c54
Marcus Seel <marcus.seel@softwareinmotion.de>
2012-04-10 14:39:37 +0200

    adds capistrano-ext gem for multistaging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,26 @@
-ssh_options[:user] = "railsprod"
-set :rails_env, 'production'
-set :application, "submarine"
-set :deploy_to, "/var/lib/submarine"
-set :use_sudo, false
+require 'capistrano/ext/multistage'
+
+set :stages, %w(production ent)
+set :default_stage, "ent"
+
 default_run_options[:pty] = true
+
+set :application, 'submarine'
+set :scm, :git
+set :repository,  "ssh://entadmin@192.168.202.4/home/Projects/#{application}.git"
+
+set :rails_env, 'production'
+set :use_sudo, false
 set :rake,      "bundle exec rake"
 
-set :scm, :git
-set :repository,  "ssh://entadmin@192.168.202.4/home/Projects/submarine.git"
 
-
-role :web, "submarine"                          # Your HTTP server, Apache/etc
-role :app, "submarine"                          # This may be the same as your `Web` server
-role :db,  "submarine", :primary => true # This is where Rails migrations will run
 
 namespace :deploy do
   task :start, :roles => [:web, :app] do 
-    run "cd #{deploy_to}/current && nohup bundle exec thin -C thin/production_config.yml -R thin/config.ru start" 
+    run "cd #{deploy_to}/current && nohup bundle exec thin -C thin/#{domain}_config.yml -R thin/config.ru start" 
   end
   task :stop, :roles => [:web, :app] do 
-    run "cd #{deploy_to}/current && bundle exec thin -C thin/production_config.yml stop"
+    run "cd #{deploy_to}/current && bundle exec thin -C thin/#{domain}_config.yml stop"
   end
   task :restart, :roles => [:web, :app], :except => { :no_release => true } do
     deploy.stop
