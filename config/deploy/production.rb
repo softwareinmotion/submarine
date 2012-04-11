b77afaa7db1e3cb0f03dd2333e420462967e9ae7
Barbara Hönle <barbara.hoenle@softwareinmotion.de>
2012-04-11 10:22:33 +0200

    Set product variant for cap tasks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 ssh_options[:user] = "railsprod"
 set :rails_env, 'production'
 set :domain, 'submarine'
+set :product_variant, 'swim'
 role :web,  domain 
 role :app,  domain 
 role :db, domain, :primary => true
