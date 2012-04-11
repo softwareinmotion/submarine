07f06bc137245bd2216e20714cf02bf0b0daf998
Barbara Hönle <barbara.hoenle@softwareinmotion.de>
2012-04-11 10:22:33 +0200

    Set product variant for cap tasks

--- config/deploy/ent.rb
+++ config/deploy/ent.rb
@@ -1,6 +1,7 @@
 ssh_options[:user] = "administrator"
 set :rails_env, 'production-ent'
 set :domain, "ent"
+set :product_variant, 'presentation'
 role :web,  domain 
 role :app,  domain 
 role :db, domain, :primary => true
