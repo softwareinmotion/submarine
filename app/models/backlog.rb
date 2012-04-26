2c00304ec3e049a13ed242d79ea47d26300c0709
Viktor Schepik <viktor.schepik@softwareinmotion.de>
2012-04-26 13:03:00 +0200

    adapt issue model to new data model

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -1,3 +1,15 @@
 class Backlog < ActiveRecord::Base
   has_many :issues
+
+  def self.backlog
+    Backlog.where(name: 'backlog').first
+  end
+
+  def self.sprint_backlog
+    Backlog.where(name: 'sprint_backlog').first
+  end
+
+  def self.finished_backlog
+    Backlog.where(name: 'finished_backlog').first
+  end
 end
---
+++ db/feature_migrate/20120426073305_create_backlogs.rb
@@ -0,0 +1,41 @@
+class CreateBacklogs < ActiveRecord::Migration
+  def up
+    create_table :backlogs do |t|
+      t.string :name
+      t.boolean :locked
+      t.string :locked_session_id
+
+      t.timestamps
+    end
+
+    add_column :issues, :backlog_id, :integer
+
+    # migrate data
+    backlog          = Backlog.create! name: "backlog", locked: false
+    sprint_backlog   = Backlog.create! name: "sprint_backlog", locked: false
+    finished_backlog = Backlog.create! name: "finished_backlog", locked: false
+
+    Issue.all.each do |issue|
+      if issue.finished
+        issue.backlog = finished_backlog
+      elsif issue.sprint_flag
+        issue.backlog = sprint_backlog
+      else
+        issue.backlog = backlog
+      end
+      issue.save!
+    end
+
+    remove_column :issues, :sprint_flag
+    remove_column :issues, :finished
+  end
+
+  def down
+    feature_active? :temp_lock_lists do
+      drop_table :backlogs
+
+      add_column :issues, :sprint_flag, :boolean
+      add_column :issues, :finished, :boolean
+    end
+  end
+end
