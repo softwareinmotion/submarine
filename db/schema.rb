f0e277b2f9aac253378f51e8bcdf7e06889843df
Viktor Schepik <viktor.schepik@softwareinmotion.de>
2012-05-09 12:57:59 +0200

    add updated schema; forgot it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416123428) do
+ActiveRecord::Schema.define(:version => 20120509064652) do
+
+  create_table "configurables", :force => true do |t|
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "configurables", ["name"], :name => "index_configurables_on_name"
 
   create_table "issues", :force => true do |t|
     t.string   "name"
