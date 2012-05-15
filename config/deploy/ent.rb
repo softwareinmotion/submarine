4c30299e50bd82c03acffac73e6beb722fbdc360
Marcus Seel <marcus.seel@softwareinmotion.de>
2012-05-15 07:26:11 +0200

    adds a attribut for deployment

--- config/deploy/ent.rb
+++ config/deploy/ent.rb
@@ -3,6 +3,7 @@ set :rails_env, 'production-ent'
 set :domain, "ent"
 set :product_variant, 'presentation'
 set :deploy_to, "/var/lib/#{application}"
+set :stage, "ent"
 role :web,  domain 
 role :app,  domain 
 role :db, domain, :primary => true
--- config/deploy/transformers.rb
+++ config/deploy/transformers.rb
@@ -3,6 +3,7 @@ set :rails_env, 'production-transformers'
 set :domain, 'submarine-transformers'
 set :product_variant, 'swim'
 set :subdomain, 'transformers_'
+set :stage, "submarine-transformers"
 set :deploy_to, "/var/lib/#{subdomain}#{application}"
 role :web,  domain 
 role :app,  domain 
