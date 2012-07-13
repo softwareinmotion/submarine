209d20496699f26610cd611a4f346e2a404dfc6f
Marcel Medak <marcel.medak@softwareinmotion.de>
2012-07-13 16:29:23 +0200

    added default value to lock_version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120705135117) do
+ActiveRecord::Schema.define(:version => 20120713130727) do
 
   create_table "backlogs", :force => true do |t|
     t.string   "name"
