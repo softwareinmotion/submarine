0d39a395a20c5cb6426a1f0954228965de850bfa
Marcus Seel <marcus.seel@softwareinmotion.de>
2013-03-05 13:20:11 +0100

    changes the ext server ip

--- config/deploy/ext.rb
+++ config/deploy/ext.rb
@@ -1,6 +1,6 @@
 ssh_options[:user] = "railsprod"
 set :rails_env, 'production-ext'
-set :domain, "10.10.50.4"
+set :domain, "10.10.50.3"
 set :product_variant, 'presentation'
 set :deploy_via, :copy
 set :copy_strategy, :export
