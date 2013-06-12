4af2de0e2eb935e28c455bf6f68cc8ae7099e7b3
Lars Dierolf <Lars.Dierolf@softwareinmotion.de>
2013-06-12 16:04:00 +0200

    Changed picture Upload url and made the icon cache available

--- app/uploaders/project_icon_uploader.rb
+++ app/uploaders/project_icon_uploader.rb
@@ -17,7 +17,7 @@ class ProjectIconUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    'public/'
+    'uploads/'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
