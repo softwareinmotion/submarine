c34ea6b0521b52227a83416afda0bccd9f676fff
Sabrina Lang <sabrina.lang@softwareinmotion.de>
2014-10-30 13:17:01 +0100

    adds specs for projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,18 @@
 # encoding: utf-8
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :destroy, :project_icon]
+
   def index
     @projects = Project.all
   end
 
   def create
     @project = Project.new(project_params)
+
     if @project.save
       redirect_to projects_path, :notice => 'Projekt erfolgreich angelegt!'
     else
-      render :action => "new"
+      render :action => 'new'
     end
   end
 
@@ -18,34 +21,34 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
-    if @project.update_attributes(project_params)
-      redirect_to projects_path, :notice => 'Erfolgreich Editiert'
+    if @project.update(project_params)
+      redirect_to projects_path, :notice => 'Erfolgreich editiert.'
     else
       render :action => 'edit'
     end
   end
 
   def project_icon
-    project = Project.find(params[:id])
-    send_data project.project_icon.read, filename: project.project_icon.file.filename
+    send_data @project.project_icon.read, filename: @project.project_icon.file.filename
   end
 
   def destroy
-    @project = Project.find(params[:id])
-    if Issue.where('project_id = ?',@project.id).where('backlog_id != ?', Backlog.finished_backlog).count == 0
+    if Issue.where('project_id = ?', @project.id).where('backlog_id != ?', Backlog.finished_backlog).count == 0
       @project.destroy
-      redirect_to projects_path, :notice => 'Erfolgreich Gelöscht'
+      redirect_to projects_path, :notice => 'Erfolgreich gelöscht.'
     else
-      redirect_to projects_path, :notice => 'Projekte können nur Gelöscht werden wenn alle Issues abgeschlossen sind!'
+      redirect_to projects_path, :notice => 'Projekte können nur gelöscht werden, wenn alle Issues abgeschlossen sind!'
     end
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit("name", "project_icon_cache", "project_icon", "remove_project_icon")
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,72 +1,178 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  before :each do
-    create(:backlog)
-    create(:backlog, name: "sprint_backlog")
-    create(:backlog, name: "finished_backlog")
-  end
-
-  let(:project) { create(:project)}
+  let(:project) { create :project }
 
   describe '#index' do
-    it "returns http success" do
+    it 'assigns all projects as @projects' do
       get :index
-      response.should be_success
+
+      expect(assigns(:projects)).to eq([project])
     end
 
-    it "offers all existing projects" do
-      create_list(:project, 3)
+    it 'renders the index view' do
       get :index
-      assigns(:projects).should eq(Project.all)
+
+      expect(response).to render_template(:index)
     end
   end
 
-  describe "#edit" do
-    it 'assigns the requested project to @project' do
-      get :edit, id: project
-      assigns(:project).should be_eql project
-    end
-  end  
+  describe '#create' do
+    context 'given valid params' do
+      let(:valid_params) {{ project: attributes_for(:project, name: 'Testprojekt') }}
 
-  describe "#new" do
-    it "renders the new template" do
-      get :new
-      response.should render_template :new
-    end
-
-    it "assigns a new project to @project" do
-      get :new
-      assigns(:project).should be_a_new(Project)
-    end
-  end
-
-  describe "#create" do
-    context "with valid params" do
-      it "creates a new Projekt" do
-        expect {
-          post :create, project: attributes_for(:project, name: "Test")
-        }.to change(Project, :count).from(0).to(1)
+      it 'creates a new project' do
+        expect { post :create, valid_params }.to change(Project, :count).by(1)
       end
 
-      it "redirects to project index page" do
-        post :create, project: attributes_for(:project, name: "Test")
-        response.should redirect_to projects_path
+      it 'assigns the new project as @project' do
+        post :create, valid_params
+
+        expect(assigns(:project)).to eq(Project.first)
+      end
+
+      it 'displays a success message' do
+        post :create, valid_params
+
+        expect(flash[:notice]).to eq('Projekt erfolgreich angelegt!')
+      end
+
+      it 'redirects to projects_path' do
+        post :create, valid_params
+
+        expect(response).to redirect_to(projects_path)
+      end
+    end
+
+    context 'given invalid params' do
+      let(:invalid_params) {{ project: attributes_for(:project, name: nil) }}
+
+      it 'does not save the project' do
+        expect { post :create, invalid_params }.to_not change(Project, :count)
+      end
+
+      it 'stores the errors to @issue' do
+        post :create, invalid_params
+
+        expect(assigns(:project)).to have(1).error_on :name
+      end
+
+      it 're-renders the new view' do
+        post :create, invalid_params
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe '#new' do
+    before :each do
+      get :new
+    end
+
+    it 'assigns a new project to @project' do
+      expect(assigns(:project)).to be_a_new(Project)
+    end
+
+    it 'renders the new view' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe '#edit' do
+    before :each do
+      get :edit, id: project.id
+    end
+
+    it 'assigns the requested project to @project' do
+      expect(assigns(:project)).to eq(project)
+    end
+
+    it 'renders the edit view' do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe '#update' do
+    context 'given valid params' do
+      let(:valid_params) {{ :id => project.id, project: attributes_for(:project, name: 'Testprojekt') }}
+
+      it 'assigns the requested project as @project' do
+        put :update, valid_params
+
+        expect(assigns(:project)).to eq(project)
+      end
+
+      it 'updates the requested project' do
+        pro = Project.new
+        Project.stub(:find).and_return(pro)
+
+        expect(pro).to receive(:update).with('name' => 'IssueX')
+
+        put :update, id: 42, project: { 'name' => 'IssueX' }
+      end
+
+      it 'displays a success message' do
+        put :update, valid_params
+
+        expect(flash[:notice]).to eq('Erfolgreich editiert.')
+      end
+
+      it 'redirects to projects_path' do
+        put :update, valid_params
+
+        expect(response).to redirect_to(projects_path)
+      end
+    end
+
+    context 'given invalid params' do
+      let(:invalid_params) {{ :id => project.id, project: attributes_for(:project, name: nil) }}
+
+      it 're-renders the edit view' do
+        put :update, invalid_params
+
+        expect(response).to render_template(:edit)
       end
     end
   end
 
   describe '#destroy' do
-    it "deletes the selected project" do
-      create(:project)
-      delete :destroy, id: project
-      expect(Project.exists?(project)).to be_false
+    before :each do
+      Project.stub(:find => project)
     end
-  end
 
-  describe '#update' do
-    it 'updates the selected project' do
-      
+    it 'calls destroy on the project' do
+      expect(project).to receive(:destroy)
+
+      delete :destroy, id: 42
+    end
+
+    it 'redirects to projects_path' do
+      delete :destroy, id: project.id
+
+      expect(response).to redirect_to(projects_path)
+    end
+
+    context 'given a destroyable project' do
+      let(:backlog) { create :backlog }
+      let(:issue) { create :issue, backlog: backlog, project: project }
+
+      it 'displays a success message' do
+        delete :destroy, id: project.id
+
+        expect(flash[:notice]).to eq('Erfolgreich gelöscht.')
+      end
+    end
+
+    context 'given an undestroyable project' do
+      let(:finished_backlog) { create :backlog, name: 'finished_backlog' }
+      let!(:issue) { create :issue, backlog: finished_backlog, project: project }
+
+      it 'displays an error message' do
+        delete :destroy, id: project.id
+
+        expect(flash[:notice]).to eq('Projekte können nur gelöscht werden, wenn alle Issues abgeschlossen sind!')
+      end
     end
   end
 end
