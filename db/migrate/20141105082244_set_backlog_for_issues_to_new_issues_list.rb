51544346a8ae38361a86031398a8f14fb2226651
Sabrina Lang <sabrina.lang@softwareinmotion.de>
2014-11-05 10:18:21 +0100

    refines last migration

--- db/migrate/20141105082244_set_backlog_for_issues_to_new_issues_list.rb
+++ db/migrate/20141105082244_set_backlog_for_issues_to_new_issues_list.rb
@@ -2,7 +2,9 @@ class SetBacklogForIssuesToNewIssuesList < ActiveRecord::Migration
   def change
     feature_active? :temp_changes_for_iso do
       Issue.where(backlog: Backlog.backlog).each do |issue|
-        issue.move_to(Backlog.new_issues_list)
+        issue.backlog = Backlog.new_issues_list
+        issue.examined_at = nil
+        issue.save!
       end
     end
   end
