509f70d3cdb070633ea9fb49ec09b4a2497494d9
Andre Busse <Andre.Busse@softwareinmotion.de>
2015-08-24 17:30:48 +0200

    delete comment stages

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141105082244) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "backlogs", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
